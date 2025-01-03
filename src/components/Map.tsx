0c059594df31ae4ccd6528bb8689110027ad5b63
siddhartha0 <siddhartha.sunuwar@gmail.com>
2025-01-03 09:50:47 +0545

    change in width & height

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,7 +4,7 @@ export const Map = () => (
   <KeplerGl
     id="foo"
     mapboxApiAccessToken="pk.eyJ1IjoieGludSIsImEiOiJjbHdhOG42OXowZHU4MmtwbGcya2V0OWlhIn0.s2k2PjrzeQd5CVU4CQs-xA"
-    width={1200}
-    height={1200}
+    width={window.innerWidth}
+    height={window.innerHeight}
   />
 );
